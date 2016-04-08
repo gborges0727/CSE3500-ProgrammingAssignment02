f42f711bcc0ffb67eee873754bed7b7148efbb70
Gabriel Borges <gborges0727@gmail.com>
2016-04-08 15:05:58 -0400

    Add base code 	-Able to read in a file and parse the strings to a list 	-Able to iterate through the list and run LCS on pairs 	-Able to return values from LCS into a list for output

--- SourceCode.py
+++ SourceCode.py
@@ -1,2 +1,23 @@
-def LCS():
-	
+# LCS Algorithm
+
+def LCS(string1, string2):
+	return (1, "hello")
+
+# Code to read the files
+
+file = open('CollectionSeqs/listSeqs-errorlow-l50.txt')
+stringList = list()
+for line in file:
+	stringList.append(line)
+
+#Code to run through LCS Algorithm and produce an output
+
+i = 0
+printList = list()
+while i < len(stringList):
+	string1 = stringList[i]
+	string2 = stringList[i+1]
+	i += 2
+	(length, string) = LCS(string1, string2)
+	printList.append((length, string))
+
